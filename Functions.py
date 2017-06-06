7de79fd51432465f4b9cc89008a26b3cd3cc87b7
WaqarXatti <wsatti.3844@gmail.com>
2017-06-06 21:19:04 +0500

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 21:06:00 2017
+
+@author: rameez
+"""
+
+print ("abs(-45) : ", abs(-45)) 
+print ("abs(100.12) : ", abs(100.12))
+
+import math   # This will import math module 
+print ("math.ceil(-45.17) : ", math.ceil(-45.17)) 
+print ("math.ceil(100.12) : ", math.ceil(100.12)) 
+print ("math.ceil(100.72) : ", math.ceil(100.72)) 
+print ("math.ceil(math.pi) : ", math.ceil(math.pi)) 
+
+
+import math   # This will import math module 
+print ("math.exp(-45.17) : ", math.exp(-45.17)) 
+print ("math.exp(100.12) : ", math.exp(100.12)) 
+print ("math.exp(100.72) : ", math.exp(100.72)) 
+print ("math.exp(math.pi) : ", math.exp(math.pi)) 
+
+
+import math   # This will import math module 
+print ("math.fabs(-45.17) : ", math.fabs(-45.17)) 
+print ("math.fabs(100.12) : ", math.fabs(100.12)) 
+print ("math.fabs(100.72) : ", math.fabs(100.72)) 
+print ("math.fabs(math.pi) : ", math.fabs(math.pi)) 
+
+
+
+import math   # This will import math module 
+print ("math.log(100.12) : ", math.log(100.12)) 
+print ("math.log(100.72) : ", math.log(100.72)) 
+print ("math.log(math.pi) : ", math.log(math.pi))
+
+
+
+import math   # This will import math module 
+print ("math.log10(100.12) : ", math.log10(100.12)) 
+print ("math.log10(100.72) : ", math.log10(100.72)) 
+print ("math.log10(119) : ", math.log10(119)) 
+print ("math.log10(math.pi) : ", math.log10(math.pi)) 
+
+
+print ("max(80, 100, 1000) : ", max(80, 100, 1000)) 
+print ("max(-20, 100, 400) : ", max(-20, 100, 400)) 
+print ("max(-80, -20, -10) : ", max(-80, -20, -10)) 
+print ("max(0, 100, -400) : ", max(0, 100, -400))
+
+
+
+print ("min(80, 100, 1000) : ", min(80, 100, 1000)) 
+print ("min(-20, 100, 400) : ", min(-20, 100, 400)) 
+print ("min(-80, -20, -10) : ", min(-80, -20, -10)) 
+print ("min(0, 100, -400) : ", min(0, 100, -400))
+
+
+
+import math   # This will import math module 
+print ("math.modf(100.12) : ", math.modf(100.12)) 
+print ("math.modf(100.72) : ", math.modf(100.72)) 
+print ("math.modf(119) : ", math.modf(119)) 
+print ("math.modf(math.pi) : ", math.modf(math.pi)) 
+
+
+import math   # This will import math module 
+print ("math.pow(100, 2) : ", math.pow(100, 2)) 
+print ("math.pow(100, -2) : ", math.pow(100, -2)) 
+print ("math.pow(2, 4) : ", math.pow(2, 4)) 
+print ("math.pow(3, 0) : ", math.pow(3, 0)) 
+
+
+
+print ("round(70.23456) : ", round(70.23456)) 
+print ("round(56.659,1) : ", round(56.659,1)) 
+print ("round(80.264, 2) : ", round(80.264, 2)) 
+print ("round(100.000056, 3) : ", round(100.000056, 3)) 
+print ("round(-100.000056, 3) : ", round(-100.000056, 3))
+
+
+
+import math   # This will import math module 
+print ("math.sqrt(100) : ", math.sqrt(100)) 
+print ("math.sqrt(7) : ", math.sqrt(7)) 
+print ("math.sqrt(math.pi) : ", math.sqrt(math.pi))
+
+
+import random 
+print ("random() : ", random.random()) 
+# Second random number 
+print ("random() : ", random.random())
+
+
+
+import random 
+list = [20, 16, 10, 5]; 
+random.shuffle(list) 
+print ("Reshuffled list : ",  list) 
+random.shuffle(list) 
+print ("Reshuffled list : ",  list)
+
+
